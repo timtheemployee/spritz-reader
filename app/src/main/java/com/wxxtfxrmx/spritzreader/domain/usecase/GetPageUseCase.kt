42edb03023a537a9a7f7626cd41f65835b85ae33
Timofey Bystrov <timapro21@gmail.com>
2020-05-03 16:49:03 +0300

    fix something on reading fragment

--- app/src/main/java/com/wxxtfxrmx/spritzreader/domain/usecase/GetPageUseCase.kt
+++ app/src/main/java/com/wxxtfxrmx/spritzreader/domain/usecase/GetPageUseCase.kt
@@ -1,14 +1,19 @@
 package com.wxxtfxrmx.spritzreader.domain.usecase
 
+import android.util.Log
 import com.itextpdf.kernel.pdf.PdfDocument
 import com.itextpdf.kernel.pdf.PdfReader
 import com.itextpdf.kernel.pdf.canvas.parser.PdfTextExtractor
-import com.itextpdf.kernel.pdf.canvas.parser.listener.SimpleTextExtractionStrategy
+import com.wxxtfxrmx.spritzreader.SpritzTextExtractionStrategy
 import com.wxxtfxrmx.spritzreader.domain.entity.Book
 import java.io.File
 import javax.inject.Inject
 
 class GetPageUseCase @Inject constructor() {
+
+    private companion object {
+        val NEW_PARAGRAPH_REGEX = "\\.\\s{3,}".toRegex()
+    }
 
     operator fun invoke(book: Book): String {
         val file = File(book.path)
@@ -18,8 +23,10 @@ class GetPageUseCase @Inject constructor() {
         val reader = PdfReader(file)
         val document = PdfDocument(reader)
 
-        val extractStrategy = SimpleTextExtractionStrategy()
+        val extractStrategy = SpritzTextExtractionStrategy()
         val page = document.getPage(book.lastPage + 1)
-        return PdfTextExtractor.getTextFromPage(page, extractStrategy)
+        return PdfTextExtractor.getTextFromPage(page, extractStrategy).replace("-", "").apply {
+            Log.e("SOME TAG", this)
+        }
     }
 }
