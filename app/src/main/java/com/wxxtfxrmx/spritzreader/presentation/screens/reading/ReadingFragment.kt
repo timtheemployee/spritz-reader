834efccb8314489749a43ff6a62a60aefbea2cdc
Timofey Bystrov <timapro21@gmail.com>
2020-05-04 15:28:13 +0300

    Added action chips on reading screen

--- app/src/main/java/com/wxxtfxrmx/spritzreader/presentation/screens/reading/ReadingFragment.kt
+++ app/src/main/java/com/wxxtfxrmx/spritzreader/presentation/screens/reading/ReadingFragment.kt
@@ -1,20 +1,28 @@
 package com.wxxtfxrmx.spritzreader.presentation.screens.reading
 
 import android.os.Bundle
+import android.os.Handler
+import android.view.MotionEvent
 import android.view.View
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import com.wxxtfxrmx.spritzreader.R
 import com.wxxtfxrmx.spritzreader.presentation.core.BaseFragment
 import kotlinx.android.synthetic.main.reading_fragment.*
 import javax.inject.Inject
 
-class ReadingFragment: BaseFragment(), ReadingView {
+class ReadingFragment : BaseFragment(), ReadingView {
 
     companion object {
         fun newInstance(): Fragment = ReadingFragment()
     }
 
     override val layout = R.layout.reading_fragment
+
+    private val chipsVisibilityHandler = Handler()
+    private val chipsVisibilityTask = Runnable {
+        actionChips.isVisible = true
+    }
 
     @Inject
     lateinit var presenter: ReadingPresenter
@@ -23,6 +31,18 @@ class ReadingFragment: BaseFragment(), ReadingView {
         super.onViewCreated(view, savedInstanceState)
 
         presenter.attachView(this)
+
+
+        contentScrollContainer.setOnTouchListener { _, event ->
+            if (event.action == MotionEvent.ACTION_DOWN) {
+                actionChips.isVisible = false
+                chipsVisibilityHandler.removeCallbacks(chipsVisibilityTask)
+            } else if (event.action == MotionEvent.ACTION_UP) {
+                chipsVisibilityHandler.postDelayed(chipsVisibilityTask, 500L)
+            }
+
+            false
+        }
     }
 
     override fun renderPage(content: String?) {
